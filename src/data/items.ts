8a1bbfd207dd72abdf534b9fa5c53fbcb893456b
alexandresaints <alexandresaintss@gmail.com>
2021-10-13 02:12:51 -0300

    in development

---
+++ src/data/items.ts
@@ -0,0 +1,7 @@
+import {Item} from '../types/Item'
+export const items: Item[] = [
+    {date: new Date(2021, 9, 15), category: 'food', title: 'McDonalds', value: 32.21},
+    {date: new Date(2021, 9, 15), category: 'food', title: 'Burger King', value: 28},
+    {date: new Date(2021, 9, 16), category: 'rent', title: 'Aluguel APT', value: 2300},
+    {date: new Date(2021, 10, 18), category: 'salary', title: 'Salário', value: 4500},
+]
